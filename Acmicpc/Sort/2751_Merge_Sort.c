95c04f531348a92b20b2211765010b632eea88f3
LeeHan-Sol <leehansol.kr@gmail.com>
2020-06-13 17:50:09 +0900

    LinkedList 정렬(Merge_Sort) 구현 완료

--- Acmicpc/Sort/2751_Merge_Sort.c
+++ Acmicpc/Sort/2751_Merge_Sort.c
@@ -4,13 +4,14 @@
 typedef struct Node
 {
 	char * data;
+	struct Node * prev;
 	struct Node * next;
 
-	void (*init)(struct Node *, char *, struct Node *);
+	void (*init)(struct Node *, char *, struct Node *, struct Node *);
 }Node;
 
-Node * createNode(char *, Node *);
-void initNode(Node *, char *, Node *);
+Node * createNode(char *, Node *, Node *);
+void initNode(Node *, char *, Node *, Node *);
 
 typedef struct List
 {
@@ -24,6 +25,7 @@ typedef struct List
 	void (*pushFront)(struct List *, int);
 	int (*popFront)(struct List *);
 	int (*peekFront)(struct List *);
+	void (*moveItem)(struct List *, Node *, Node *);
 }List;
 
 List * createList();
@@ -33,10 +35,11 @@ void displayList(List *);
 void pushFront(List *, int);
 int popFront(List *);
 int peekFront(List *);
+void moveItem(List *, Node *, Node *);
 
-void sort(List *, int, int);
+int sort(List *, int, int);
 void swap(int *, int *);
-Node * divide(List *, int, int);
+int divide(List *, int, int);
 Node * conquer(Node *, Node *, int);
 
 int main()
@@ -55,24 +58,25 @@ int main()
 		list->pushFront(list, input);
 	}
 
-	list->display(list);
+//	list->display(list);
 
 	sort(list, 0, list->size-1);
+	list->display(list);
 
 	return 0;
 }
 
-Node * createNode(char * data, Node * next)
+Node * createNode(char * data, Node * prev, Node * next)
 {
 	Node * new_node = (Node *)malloc(sizeof(Node));
 	
 	new_node->init = initNode;
-	new_node->init(new_node, data, next);
+	new_node->init(new_node, data, prev, next);
 
 	return new_node;
 }
 
-void initNode(Node * node, char * data, Node * next)
+void initNode(Node * node, char * data, Node * prev, Node * next)
 {
 	node->data = (char *)malloc(sizeof(char) * 4);
 	if(node->data == NULL)
@@ -81,6 +85,7 @@ void initNode(Node * node, char * data, Node * next)
 		exit(1);
 	}
 	*((int *)node->data) = *((int *)data);
+	node->prev = prev;
 	node->next = next;
 
 	return ;
@@ -113,6 +118,7 @@ void initList(List * list)
 	list->pushFront = pushFront;
 	list->popFront = popFront;
 	list->peekFront = peekFront;
+	list->moveItem = moveItem;
 
 	return ;
 }
@@ -129,12 +135,14 @@ void displayList(List * list)
 		return ;
 	}
 
-	fputs("List 항목\n", stdout);
+//	fputs("List 항목\n", stdout);
 	for(Node * temp = list->head; temp != NULL; temp = temp->next)
 	{
-		fprintf(stdout, "%5d", *((int *)temp->data));	
+		fprintf(stdout, "%d\n", *((int *)temp->data));
+//		fprintf(stdout, "%5d", *((int *)temp->data));	
+//		fprintf(stdout, "%5d[%p]", *((int *)temp->data), temp);	
 	}
-	fputc('\n', stdout);
+//	fputc('\n', stdout);
 
 	return ;
 }
@@ -143,14 +151,15 @@ void pushFront(List * list, int data)
 {
 	if(list->isEmpty(list))
 	{
-		list->head = createNode((char *)&data, NULL);
+		list->head = createNode((char *)&data, NULL, NULL);
 		list->tail = list->head;
 		list->size++;
 		
 		return ;
 	}
 
-	list->head = createNode((char *)&data, list->head);
+	list->head->prev = createNode((char *)&data, NULL, list->head);
+	list->head = list->head->prev;
 	list->size++;
 
 	return ;
@@ -165,6 +174,7 @@ int popFront(List * list)
 
 	Node * temp = list->head;
 	list->head = list->head->next;
+	list->head->prev = NULL;
 	if(list->head == NULL)
 	{
 		list->tail = NULL;
@@ -179,6 +189,28 @@ int popFront(List * list)
 int peekFront(List * list)
 {
 	return *((int *)list->head->data);
+}
+
+void moveItem(List * list, Node * from, Node * to)
+{
+	if(from->next != NULL)
+		from->next->prev = from->prev;
+	from->prev->next = from->next;
+	from->next = from->prev = NULL;
+
+	from->next = to;
+	if(to->prev != NULL)
+	{
+		to->prev->next = from;
+	}
+	from->prev = to->prev;
+	to->prev = from;
+	if(list->head == to)
+	{
+		list->head = from;
+	}
+
+	return ;
 }
 
 void displayArray(int * array, int size)
@@ -197,11 +229,65 @@ void displayArray(int * array, int size)
 	return ;
 }
 
-void sort(List * list, int left, int right)
+int sort(List * list, int start, int end)
 {	
-	divide(list, left, right);
+//	fprintf(stdout, "start, end : %d, %d\n", start, end);
+	if(start == end) return 1;
+	
+	int mid = (start + end) / 2;
 
-	return ;
+	int compare_left = 0;
+	int compare_right = 0;
+	
+	int left = sort(list, start, mid);
+	int right = sort(list, mid + 1, end);
+//	fprintf(stdout, "left, right: %d, %d\n", left, right);
+
+	Node * node_left = list->head;
+	Node * node_right = list->head;
+
+	for(int i = 0; i < start; i++)
+	{
+		node_left = node_left->next;		
+	}
+	for(int i = 0; i < end - (right - 1); i++)
+	{
+		node_right = node_right->next;
+	}
+
+	for(;;)
+	{
+//	fprintf(stdout, "node_left, node_right : %d, %d", *((int *)node_left->data), *((int *)node_right->data));
+//	fprintf(stdout, "\t\tstart, end : %d, %d\n", start, end);
+
+		if(left == compare_left || right == compare_right) 
+		{
+//			fputs("break\n", stdout);
+			break;
+		}
+
+		if(*((int *)node_left->data) > *((int *)node_right->data))
+		{
+			if(node_right->next != NULL)
+			{
+				node_right = node_right->next;
+				list->moveItem(list, node_right->prev, node_left);
+			}
+			else
+			{
+				list->moveItem(list, node_right, node_left);
+			}
+			compare_right++;
+		}
+		else
+		{
+			node_left = node_left->next;
+			compare_left++;
+		}
+//	list->display(list);
+	}
+
+	return left + right;
 }
 
 void swap(int * a, int * b)
@@ -213,26 +299,9 @@ void swap(int * a, int * b)
 	return ;
 }
 
-Node * divide(List * list, int left, int right)
+int divide(List * list, int left, int right)
 {
-	if(left >= right) 
-	{
-		Node * result = list->head;
-		for(int i = 0; result != NULL && i < left; i++)
-		{
-			result = result->next;
-		}
-		fprintf(stdout, "마지막 남은 노드 : %d[%p]\n", *((int *)result->data), result);
-		return result;
-	}
-
-	int mid = (left + right) / 2;
-
-	Node * node_left = divide(list, left, mid);
-	Node * node_right = divide(list, mid + 1, right);
-
-	int count = right - left + 1;
-	conquer(node_left, node_right, count);
+	return 0;
 }
 
 Node * conquer(Node * left, Node * right, int count)
